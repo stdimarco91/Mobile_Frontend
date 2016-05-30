cb5e83b3c65c3b9dddb1e9a4594be1636d55658d
Stefano <stefano-dimarco91@virgilio.it>
2016-05-30 11:40:10 +0200

    Start

--- www/smartphone/js/config.js
+++ www/smartphone/js/config.js
@@ -2,4 +2,4 @@
  * Created by Stefano on 13/05/2016.
  */
 
-var serverUrl = "http://artia.ddns.net:8080/SmartHouse_war_exploded";
+var serverUrl = "http://10.52.15.177:8081/SmartHouse_war_exploded";
